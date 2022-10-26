25272a329a108cb69c9e9900bf2c630b78c9281a
NoahSchiro <noahschiro@rocketmail.com>
2022-10-26 15:21:39 -0400

    Updated knight movements

--- main.cpp
+++ main.cpp
@@ -11,26 +11,9 @@ int main() {
 	myBoard.cliShow();
 
 	//Access the kings moves
-	std::vector<Coordinates> moves = myBoard.m_board[7][4]->generateValidMoves(myBoard.m_board);
+	std::vector<Coordinates> moves = myBoard.m_board[7][1]->generateValidMoves(myBoard.m_board);
 
 	for(auto it : moves) {
-		int xPos = it.x;
-		int yPos = it.y;
-		std::cout << "(" << xPos << ", " << yPos << ")\n";
-	}
-
-	Coordinates from(4, 7);
-	Coordinates to  (4, 4);
-
-	myBoard.movePiece(from, to);
-	myBoard.cliShow();
-
-	std::cout << "Move 1:\n";
-
-	//Access the kings moves
-	std::vector<Coordinates> moves2 = myBoard.m_board[4][4]->generateValidMoves(myBoard.m_board);
-
-	for(auto it : moves2) {
 		int xPos = it.x;
 		int yPos = it.y;
 		std::cout << "(" << xPos << ", " << yPos << ")\n";
--- pieces/knight.cpp
+++ pieces/knight.cpp
@@ -29,56 +29,96 @@ std::vector<Coordinates> Knight::generateValidMoves(Piece* board[8][8]) {
 	if(m_position.x > 1 && m_position.y < 7) {
 		temp.x = m_position.x - 2;
 		temp.y = m_position.y + 1;
-		ans.push_back(temp);
+
+		//Only a valid move if the space is empty
+		//or it's the opposite color
+		if(board[temp.y][temp.x]->getColor() != m_color) {
+			ans.push_back(temp);
+		}
 	}
 
 	//Left top
 	if(m_position.x > 1 && m_position.y > 0) {
 		temp.x = m_position.x - 2;
 		temp.y = m_position.y - 1;
-		ans.push_back(temp);	
+
+		//Only a valid move if the space is empty
+		//or it's the opposite color
+		if(board[temp.y][temp.x]->getColor() != m_color) {
+			ans.push_back(temp);
+		}
 	}
 
 	//Top left
 	if(m_position.x > 0 && m_position.y > 1) {
 		temp.x = m_position.x - 1;
 		temp.y = m_position.y - 2;
-		ans.push_back(temp);
+
+		//Only a valid move if the space is empty
+		//or it's the opposite color
+		if(board[temp.y][temp.x]->getColor() != m_color) {
+			ans.push_back(temp);
+		}
 	}
 
 	//Top right
 	if(m_position.x < 7 && m_position.y > 1) {
 		temp.x = m_position.x + 1;
 		temp.y = m_position.y - 2;
-		ans.push_back(temp);	
+
+		//Only a valid move if the space is empty
+		//or it's the opposite color
+		if(board[temp.y][temp.x]->getColor() != m_color) {
+			ans.push_back(temp);
+		}
 	}
 
 	//Right top
 	if(m_position.x < 6 && m_position.y > 0) {
 		temp.x = m_position.x + 2;
 		temp.y = m_position.y - 1;
-		ans.push_back(temp);
+
+		//Only a valid move if the space is empty
+		//or it's the opposite color
+		if(board[temp.y][temp.x]->getColor() != m_color) {
+			ans.push_back(temp);
+		}
 	}
 
 	//Right bottom
 	if(m_position.x < 6 && m_position.y < 7) {
 		temp.x = m_position.x + 2;
 		temp.y = m_position.y + 1;
-		ans.push_back(temp);	
+
+		//Only a valid move if the space is empty
+		//or it's the opposite color
+		if(board[temp.y][temp.x]->getColor() != m_color) {
+			ans.push_back(temp);
+		}
 	}
 
 	//Bottom right
 	if(m_position.x < 7 && m_position.y < 6) {
 		temp.x = m_position.x + 1;
 		temp.y = m_position.y + 2;
-		ans.push_back(temp);
+
+		//Only a valid move if the space is empty
+		//or it's the opposite color
+		if(board[temp.y][temp.x]->getColor() != m_color) {
+			ans.push_back(temp);
+		}
 	}
 
 	//Bottom left
 	if(m_position.x > 0 && m_position.y < 6) {
 		temp.x = m_position.x - 1;
 		temp.y = m_position.y + 2;
-		ans.push_back(temp);
+
+		//Only a valid move if the space is empty
+		//or it's the opposite color
+		if(board[temp.y][temp.x]->getColor() != m_color) {
+			ans.push_back(temp);
+		}
 	}
 
 	return ans;
